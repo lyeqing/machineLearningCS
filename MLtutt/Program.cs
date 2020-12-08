f76d9c93347ba78aad35960fd6ee2b3fdf5311b2
Louis Liu <lyeqing@ymail.com>
2020-12-08 21:29:04 +1030

    machine learning of c#, python is better

---
+++ MLtutt/Program.cs
@@ -0,0 +1,19 @@
+﻿using MLtuttML.Model;
+using System;
+
+namespace MLtutt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "That is rude.";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
